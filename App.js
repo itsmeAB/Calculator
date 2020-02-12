f2b6f58efa93d4adcf51b8993af61cf8219b57fd
ABHILASH <ss.abhilash3@gmail.com>
2020-02-13 00:31:36 +0530

    wip - add layout and buttons

--- App.js
+++ App.js
@@ -1,19 +1,105 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
 
 export default function App() {
+  let rows = [];
+  let nums = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [0, 0, '=']
+  ];
+  for (let i = 0; i < 4; i++) {
+    let row = [];
+    for (let j = 0; j < 3; j++) {
+      row.push(
+        <TouchableOpacity style={styles.btn}>
+          <Text style={styles.btnText}>{nums[i][j]}</Text>
+        </TouchableOpacity>
+      );
+    }
+    rows.push(<View style={styles.row}>{row}</View>);
+  }
+
+  let operations = ['+', '-', '*', '/'];
+  let ops = [];
+  for (let i = 0; i < 4; i++) {
+    ops.push(
+      <TouchableOpacity style={styles.btn}>
+        <Text style={(styles.btnText, styles.white)}>{operations[i]}</Text>
+      </TouchableOpacity>
+    );
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.result}>
+        <Text style={styles.resultText}>11*11</Text>
+      </View>
+      <View style={styles.calculation}>
+        <Text style={styles.calculationText}>122</Text>
+      </View>
+      <View style={styles.buttons}>
+        <View style={styles.numbers}>{rows}</View>
+        <View style={styles.operations}>{ops}</View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1
   },
+  btnText: {
+    fontSize: 30
+  },
+  white: {
+    color: 'white'
+  },
+  btn: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'stretch'
+  },
+  resultText: {
+    fontSize: 30,
+    color: 'white'
+  },
+  calculationText: {
+    fontSize: 24,
+    color: 'white'
+  },
+  row: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+  result: {
+    flex: 2,
+    justifyContent: 'center',
+    alignItems: 'flex-end',
+    backgroundColor: 'red'
+  },
+  calculation: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'flex-end',
+    backgroundColor: 'green'
+  },
+  buttons: {
+    flexGrow: 7,
+    flexDirection: 'row'
+  },
+  numbers: {
+    flex: 3,
+    backgroundColor: 'yellow'
+  },
+  operations: {
+    flex: 1,
+    justifyContent: 'space-around',
+    alignItems: 'stretch',
+    backgroundColor: 'black'
+  }
 });
